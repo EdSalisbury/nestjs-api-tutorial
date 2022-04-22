527e0e9bd4453020375c0a7ccd2752444bb6d071
Ed Salisbury <ed.salisbury@gmail.com>
2022-04-21 19:11:21 -0600

    Added getUser decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,17 @@
 import {
   Controller,
   Get,
-  Req,
   UseGuards,
 } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Request } from "express";
+
+import { User } from "@prisma/client";
+import { GetUser } from "../auth/decorator";
 import { JwtGuard } from "../auth/guard";
 @Controller("users")
 export class UserController {
   @UseGuards(JwtGuard)
   @Get("me")
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
